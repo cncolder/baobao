776b682fb1bce1c0934e6945339896e869abe383
colder <cncolder@gmail.com>
2015-09-25 09:38:37 +0800

    contents finished.
    portfolio 4-6.
    join us details.

--- public/js/index.js
+++ public/js/index.js
@@ -1,10 +1,24 @@
+// Tooltip startup.
+
 $('[data-toggle=tooltip]').tooltip();
+
+
+// Wechat cannot click. Only show a barcode.
 
 $('#wechat_barcode')
   .click(function() {
     return false;
-  })
-  .tooltip();
+  });
 
-$('#weibo_account')
-  .tooltip();
+
+// Here is entrance for editor.
+
+var copyrightClickTimes = 0;
+
+$('#copyright')
+  .click(function() {
+    if (++copyrightClickTimes >= 7) {
+      copyrightClickTimes = 0;
+      alert('Welcome editor.');
+    }
+  });
